1657155774dbc53b3a2c97b935b0ccf5f22f01f6
Michał Brusiło <mibru@interia.pl>
2018-04-04 10:31:19 +0200

    Update index.html and scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,7 @@
 /* skrypt do obliczania
 pola trójkąta*/
+var a = prompt("Podaj wartość a");
+var h = prompt("Podaj wartość h");
+var triangleArea = a*h/2;
+document.getElementById("triangleArea").innerHTML =('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
+console.log(triangleArea);
